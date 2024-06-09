d1368e1d581be1ab533e7bca24787d1d816534a4
ln613 <ln613@hotmail.com>
2024-06-08 21:50:44 -0700

    env

--- netlify/functions/utils/http.js
+++ netlify/functions/utils/http.js
@@ -34,10 +34,9 @@ export const res = (body, code, nocache) => ({
 export const makeApi =
   ({ handlers, connectDB, initAI, nocache }) =>
   async (event, context) => {
-    //console.log(event)
     context.callbackWaitsForEmptyEventLoop = false
     const q = event.queryStringParameters
-    const method = 'get' //event.httpMethod.toLowerCase()
+    const method = event.httpMethod.toLowerCase()
     const body = method === 'post' && tryc(() => JSON.parse(event.body))
     origin = event.rawUrl.slice(0, event.rawUrl.indexOf(FUNC) + FUNC.length)
 
