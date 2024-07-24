f9f1ee6bf2f71512938cde7e53deb9a2be51734e
Nan Li <ln613@hotmail.com>
2024-07-24 12:05:19 -0700

    Revert "Update auth.dart"
    This reverts commit 2019616ee24dc8faba8e3d6af5ad65af4836fd0a.

--- lib/api/auth.dart
+++ lib/api/auth.dart
@@ -1,14 +1,7 @@
-import 'package:chatuni/widgets/common/snack.dart';
-
 import '/models/user.dart';
 import 'api.dart';
 
 final post = dioPost(vipBase);
-final headers = {
-  'Content-Type': 'application/json',
-  // 'Authorization':
-  //    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOjksInBob25lIjoiMTcxMDE4MzI3MjIiLCJpYXQiOjE3MTg2MTY4ODYsImV4cCI6MTcyMTIwODg4Nn0.gPZYtavTYIOSo_Dhpkcup1ae_3grI3RiZs_IpjI-3NM',
-};
 
 // Future<List<Tutor>> getToken() async {
 //   final r = await get('tutor', params: {'type': 'tutors'});
@@ -16,78 +9,15 @@ final headers = {
 // }
 
 final phoneData =
-    (String phone) => {'countrycode': '+1', 'identifier': phone, 'type': '1'};
-
-final emailData = (String email) => {
-      'identifier': email,
-      'type': '1',
-    };
-String? validatePhoneNumber(String phoneNumber) {
-  final RegExp regex = RegExp(r'(^(?:[+0]9)?[0-9]{10,12}$)');
-  if (!regex.hasMatch(phoneNumber)) {
-    return 'Enter a valid phone number';
-  }
-  return null;
-}
-
-String? validateEmail(String value) {
-  RegExp regex = RegExp(
-    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
-  );
-  if (!regex.hasMatch(value)) {
-    return 'Enter Valid Email';
-  }
-  return null;
-}
-
-Future<bool> validateOTP(String phone, String code) async {
-  var r = await post(
-    'login/verify_code',
-    data: {
-      'phone': phone,
-      'code': code,
-    },
-    headers: headers,
-  );
-  print(r['result']);
-
-  if (r['result'] == 'Code valid') {
-    return true;
-  } else {
-    return false;
-  }
-}
+    (String phone) => {'countrycode': '+86', 'identifier': phone, 'type': '1'};
 
 Future<String?> sendCodeToPhone(String phone, [String type = '1']) async {
-  final error = validatePhoneNumber(phone);
-  if (error != null) {
-    snack('Please enter a valid number');
-  } else {
-    snack('Code sent!');
-    var r = await post(
-      'login/authcode',
-      data: phoneData(phone),
-      headers: headers,
-    );
-    return r['result'];
-  }
-  return null;
-}
-
-Future<String?> sendCodeToEmail(String email, [String type = '1']) async {
-  final error = validateEmail(email);
-  if (error != null) {
-    snack('Please enter a valid email');
-  } else {
-    snack('Code sent!');
-    var r = await post(
-      'login/authcode',
-      data: emailData(email),
-      headers: headers,
-    );
-    return r['result'];
-  }
-  return null;
+  var r = await post(
+    'login/authcode',
+    data: phoneData(phone),
+    headers: headers,
+  );
+  return r['result'];
 }
 
 Future<User?> loginWithPhoneCode(String phone, String code) async {
@@ -101,6 +31,5 @@ Future<User?> loginWithPhoneCode(String phone, String code) async {
     },
     headers: headers,
   );
-  snack('Login successful!');
   return User.fromJson(r['result']);
 }
