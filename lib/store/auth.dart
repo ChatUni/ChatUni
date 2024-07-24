190eb34c92af2db3ee26ed7cf476da4cb270c99a
Molly Huang <48228352+MollyAredtana@users.noreply.github.com>
2024-07-24 08:17:04 -0400

    Update auth.dart
    updated some functions here

--- lib/store/auth.dart
+++ lib/store/auth.dart
@@ -1,3 +1,4 @@
+import 'package:chatuni/widgets/common/snack.dart';
 import 'package:mobx/mobx.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -19,6 +20,9 @@ abstract class _Auth with Store {
 
   @observable
   String phone = '';
+
+  @observable
+  String email = '';
 
   @observable
   String countryCode = countryCodes.first;
@@ -53,6 +57,11 @@ abstract class _Auth with Store {
   }
 
   @action
+  void setEmail(String value) {
+    email = value;
+  }
+
+  @action
   void setCountryCode(String value) {
     countryCode = value;
   }
@@ -74,6 +83,18 @@ abstract class _Auth with Store {
     isLoggingIn = false;
     isLoggedIn = true;
   }
+
+  // @action
+  // Future<void> login() async {
+  //   isLoggingIn = true;
+  //   if (await validateOTP(phone, code)) {
+  //     user = await loginWithPhoneCode(phone, code);
+  //     isLoggedIn = user != null;
+  //   } else {
+  //     snack('Invalid code. Please try again.');
+  //   }
+  //   isLoggingIn = false;
+  // }
 
   @action
   void logout() {
