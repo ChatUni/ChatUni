14915d38be6b277cd77ca047eaf400c60da006bd
Nan Li <ln613@hotmail.com>
2024-08-11 12:09:35 -0700

    course

--- lib/api/course.dart
+++ lib/api/course.dart
@@ -5,7 +5,9 @@ final get = dioGet(cuBase);
 final post = dioPost(cuBase);
 final vipPost = dioPost(vipBase);
 
-Future<List<Listening>> fetchIelts() async {
-  final r = await get('ielts');
+enum CourseType { ielts, middle, high }
+
+Future<List<Listening>> fetchListenings(CourseType type) async {
+  final r = await get(type.toString());
   return (r as List).map((t) => Listening.fromJson(t)).toList();
 }
