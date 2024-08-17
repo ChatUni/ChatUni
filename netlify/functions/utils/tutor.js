cdba97a960069bf2e282aa21ca4a8c235de8f6e5
ln613 <ln613@hotmail.com>
2024-08-16 21:05:07 -0700

    sort

--- netlify/functions/utils/tutor.js
+++ netlify/functions/utils/tutor.js
@@ -1,8 +1,9 @@
 import { DB, get, post } from './http'
+import { sortBy } from 'lodash'
 
 const db = DB('algo.ChatUni')
 
-export const getTutors = () => get(db('doc', 'tutors'))
+export const getTutors = () => get(db('doc', 'tutors')).then(r => sortBy(r, ['id']))
 
 export const saveChatId = async (id, chatId) => {
   const tutor = await get(`${db('getById', 'tutors')}&params={"id":${id}}`)
