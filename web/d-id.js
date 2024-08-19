06b0a286f8cc1bfd9f7cae3e6d67156917b63873
ln613 <ln613@hotmail.com>
2024-08-19 16:34:48 -0700

    msg

--- web/d-id.js
+++ web/d-id.js
@@ -60,7 +60,7 @@ async function createPeerConnection(offer, iceServers) {
       "created_at": new Date().toISOString()
     })
     // channel.publish('a', tutor.greetings)
-    fetch(`${chatuni_url}/api?type=pusher&channel=did&event=a`, { method: 'POST', body: tutor.greetings });
+    fetch(`${chatuni_url}/api?type=pusher&channel=did&event=a`, { method: 'POST', body: { msg: tutor.greetings } });
   };
 
   // Agent Text Responses - Decoding the responses, pasting to the HTML element
@@ -76,7 +76,7 @@ async function createPeerConnection(offer, iceServers) {
         "created_at": new Date().toISOString()
       })
       // channel.publish('a', msg)
-      fetch(`${chatuni_url}/api?type=pusher&channel=did&event=a`, { method: 'POST', body: msg });
+      fetch(`${chatuni_url}/api?type=pusher&channel=did&event=a`, { method: 'POST', body: { msg } });
     }
   };
 
