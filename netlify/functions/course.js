81f7c3d9bb2d7cfc3f8428bb26e7636328a8b353
ln613 <ln613@hotmail.com>
2024-11-09 01:58:15 -0800

    save result api

--- netlify/functions/course.js
+++ netlify/functions/course.js
@@ -1,12 +1,17 @@
 import { makeApi } from './utils/http'
-import { getIelts, getSAT } from './utils/course'
+import { getIelts, getSAT, saveResult } from './utils/course'
+import { score } from './utils/gemini'
 
 export const handler = makeApi({
   handlers: {
     get: {
       ielts: q => getIelts(),
       sat: q => getSAT(),
+      score: q => score(q.msg),
     },
+    post: {
+      saveResult: (q, b) => saveResult(b.result),
+    }
   },
   nocache: true,
 })
--- netlify/functions/utils/course.js
+++ netlify/functions/utils/course.js
@@ -3,3 +3,9 @@ import { DB, get, post } from './http'
 
 export const getIelts = () => get(DB('doc', 'ielts')).then(r => sortBy(r, 'id'))
 export const getSAT = () => get(DB('doc', 'sat')).then(r => sortBy(r, 'id'))
+
+export const saveResult = async result => {
+  if (!result.id) result.id = (await get(DB('maxId', 'result'))) + 1
+  await post(DB('save', 'result'), result)
+  return result
+}
