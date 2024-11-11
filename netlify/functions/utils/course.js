e9eea3b1789fa2b896736878ac45bd53f709c163
ln613 <ln613@hotmail.com>
2024-11-11 11:08:44 -0800

    replace

--- netlify/functions/utils/course.js
+++ netlify/functions/utils/course.js
@@ -1,11 +1,11 @@
 import { sortBy } from 'lodash'
-import { get, maxId, save } from './db'
+import { get, maxId, replace } from './db'
 
 export const getIelts = () => get('ielts').then(r => sortBy(r, 'id'))
 export const getSAT = () => get('sat').then(r => sortBy(r, 'id'))
 
 export const saveResult = async result => {
   if (!result.id) result.id = (await maxId('result')) + 1
-  await save('result', result)
+  await replace('result', result)
   return result
 }
--- netlify/functions/utils/tutor.js
+++ netlify/functions/utils/tutor.js
@@ -1,10 +1,10 @@
 import { sortBy } from 'lodash'
-import { get, getById, save } from './db'
+import { get, getById, replace } from './db'
 
 export const getTutors = () => get('tutors').then(r => sortBy(r, [x => x.lang === 'English' ? 0 : 1, 'id']))
 
 export const saveChatId = async (id, chatId) => {
   const tutor = await getById('tutors', id)
   tutor.chatId = chatId
-  await save('tutors', tutor)
+  await replace('tutors', tutor)
 }
