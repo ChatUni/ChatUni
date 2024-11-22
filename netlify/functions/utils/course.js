488f2075f79c78e0700f48a98b0ad3c85f559700
ln613 <ln613@hotmail.com>
2024-11-22 06:17:26 -0800

    order

--- netlify/functions/utils/course.js
+++ netlify/functions/utils/course.js
@@ -1,8 +1,8 @@
 import { sortBy, orderBy } from 'lodash'
 import { get, maxId, replace } from './db'
 
-export const getIelts = () => get('ielts').then(r => sortBy(r, 'id'))
-export const getSAT = () => get('sat').then(r => sortBy(r, 'id'))
+export const getIelts = () => get('ielts').then(r => orderBy(r, [x => x.id.split('-')[0], x => x.id.split('-')[1]], ['desc', 'asc']))
+export const getSAT = () => get('sat').then(r => orderBy(r, 'id', 'desc'))
 export const getResults = id => get('result').then(r => orderBy(r.filter(x => x.userId == id), 'id', 'desc'))
 
 export const saveResult = async result => {
